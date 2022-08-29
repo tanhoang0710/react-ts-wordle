2cfab87f6b535fee205f62304e9d603db4b6dd23
tanhoang0710 <tanhoang0710@gmail.com>
2022-08-29 16:25:06 +0700

    part 1

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { rootState } from '../../interface';
+import { decPos, setBoard } from '../../redux/boardSlice';
 import Key from '../Key/Key';
+import './Keyboard.css';
 
 const Keyboard: React.FC = () => {
 	const rows: string[] = [
@@ -8,11 +12,32 @@ const Keyboard: React.FC = () => {
 		'z x c v b n m',
 	];
 
+	const dispatch = useDispatch();
+
+	const board = useSelector((state: rootState) => state.board.board);
+	const pos = useSelector((state: rootState) => state.board.pos);
+
+	const clickBack = () => {
+		if (pos <= 0) return;
+		const newBoard = [...board];
+		newBoard[pos - 1] = '';
+		dispatch(decPos());
+		dispatch(setBoard(newBoard));
+	};
+
 	return (
 		<div className="keyboard-container">
 			{rows.map((row, idx) => (
-				<div className="row">
-					<Key letter={row} />
+				<div className="row" key={idx}>
+					{idx === 2 && <span className="letter-row">Enter</span>}
+					{row.split(' ').map((letter, idx) => (
+						<div className="letter-row">
+							<Key letter={letter.toUpperCase()} key={idx} />
+							{letter === 'm' && (
+								<span onClick={clickBack}>Back</span>
+							)}
+						</div>
+					))}
 				</div>
 			))}
 		</div>
